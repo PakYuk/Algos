78cc83e480a173ab2e4deabc80c717d34e93d81a
Boxu Fan <boxufan@gmail.com>
2022-01-22 21:08:04 -0600

    add: Add two digits

---
+++ Prep/Add Two Digits.js
@@ -0,0 +1,14 @@
+// You are given a two-digit integer n. Return the sum of its digits.
+
+// Example
+
+// For n = 29, the output should be
+// solution(n) = 11.
+function solution(n) {
+	let num = n.toString();
+	let sum = 0;
+	for (let i = 0; i < num.length; i++) {
+		sum += Number(num[i]);
+	}
+	return sum;
+}
