f5138804219737f46dd50eb1582dfd38634bf5da
Boxu Fan <boxufan@gmail.com>
2022-02-09 19:19:49 -0600

    add: valid parentheses

---
+++ String/Valid Parentheses.js
@@ -0,0 +1,49 @@
+/**
+ *Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+ */
+var isValid = function (s) {
+	let stack = [];
+
+	for (let i = 0; i < s.length; i++) {
+		let brackets = s[i];
+		switch (brackets) {
+			case '(':
+				stack.push(')');
+				break;
+			case '{':
+				stack.push('}');
+				break;
+			case '[':
+				stack.push(']');
+				break;
+			default:
+				if (s[i] !== stack.pop()) return false;
+		}
+	}
+	return stack.length === 0;
+};
