10447103c560516578fa339d03cf24ffa3e40047
Boxu Fan <boxufan@gmail.com>
2022-02-22 18:42:13 -0600

    add: First unique character

---
+++ String/First Unique Character in a String.js
@@ -0,0 +1,38 @@
+/**
+
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function (s) {
+	const repeated = [{}];
+	for (let i = 0; i < s.length; i++) {
+		if (s[i] in repeated) {
+			repeated[s[i]]++;
+		} else {
+			repeated[s[i]] = 1;
+		}
+	}
+
+	for (let i = 0; i < s.length; i++) {
+		if (repeated[s[i]] === 1) return i;
+	}
+	return -1;
+};
