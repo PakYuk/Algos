e485b7a59dcec1261697c21623931273a586bbf4
Boxu Fan <boxufan@gmail.com>
2022-03-14 21:44:12 -0500

    add: single number

---
+++ Arrays/136.Single Number.js
@@ -0,0 +1,47 @@
+/*
+136. Single Number
+Easy
+
+9309
+
+328
+
+Add to List
+
+Share
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,1]
+Output: 1
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+Output: 4
+Example 3:
+
+Input: nums = [1]
+Output: 1
+ 
+*/
+//hashmap
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+	const visited = {};
+	for (let value of nums) {
+		if (visited[value]) {
+			delete visited[value];
+		} else {
+			visited[value] = 1;
+		}
+	}
+	return Object.keys(visited)[0];
+};
