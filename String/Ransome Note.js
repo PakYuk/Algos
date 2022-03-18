8ed730e3c2d459097561d27ce468f8701dd0e8c2
Boxu Fan <boxufan@gmail.com>
2022-03-18 16:26:05 -0500

    add: Ransome Note

---
+++ String/Ransome Note.js
@@ -0,0 +1,40 @@
+/**
+
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+ */
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+	const map = {};
+	for (let letter of magazine) {
+		if (!map[letter]) map[letter] = 0;
+		map[letter]++;
+	}
+
+	for (let letter of ransomNote) {
+		if (!map[letter]) return false;
+		map[letter]--;
+	}
+
+	return true;
+};
